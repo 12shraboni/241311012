b753e2cc691a04e17cee1e211bd6adb823090535
12shraboni <acharjeeshraboni0@gmail.com>
2025-08-14 23:30:38 +0600

    yes commit

---
+++ 04.py
@@ -0,0 +1,7 @@
+char=input("Enter a character:")
+if char.isupper():
+    print("The character is uppercase.")
+elif char.islower():
+    print("The character is lowercase.")
+else:
+    print("The character is not an alphabet character.")
